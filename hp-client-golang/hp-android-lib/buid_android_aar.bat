8a9bf38cc22ac728e1c24b17c0b39fcc4ad7d812
北啊呢 <177828525@qq.com>
2026-07-20 22:03:06 +0800

    build(android): 统一修改安卓aar构建脚本的SDK路径
    将两个构建脚本的安卓SDK路径统一修改为当前开发者的本地路径，同时添加@echo off关闭命令执行时的回显

--- hp-client-golang/hp-android-lib/buid_android_aar.bat
+++ hp-client-golang/hp-android-lib/buid_android_aar.bat
@@ -1,5 +1,6 @@
-SET ANDROID_HOME=D:\android_sdk
-SET ANDROID_NDK_HOME=D:\android_sdk\ndk\29.0.14206865
+@echo off
+SET ANDROID_HOME=C:\Users\17782\AppData\Local\Android\Sdk
+SET ANDROID_NDK_HOME=C:\Users\17782\AppData\Local\Android\Sdk\ndk\29.0.14206865
 go install golang.org/x/mobile/cmd/gomobile
 go get golang.org/x/mobile/bind
 gomobile init
